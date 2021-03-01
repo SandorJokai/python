d9df6125828c973bfd39b17b4549ebfe4767cc0b
SandorJokai <60708018+SandorJokai@users.noreply.github.com>
2021-03-01 14:17:35 +0000

    Add files via upload
    This is the first prototype of my cv-maker program. (Herited from the previous version script) It provides a user-friendly form to ask all the questions and then create a cv for the user automatically.

---
+++ cv-builder-03.py
@@ -0,0 +1,150 @@
+#/usr/bin/python3
+# https://www.youtube.com/watch?v=mJEpimi_tFo&t=5363s
+# Before we go any further... pip install python-docx -> to this project below, we only need this lib.
+
+from docx import Document
+from docx.shared import Inches
+from tkinter import *
+
+
+def command():
+    document = Document()
+    document.add_picture(e1.get(), width=Inches(1.5))
+    document.add_paragraph(e2.get() + '\n' + 'Mobile: ' + e3.get() +
+                           '\n' + 'Email: ' + e4.get())
+    document.add_heading('ABOUT ME')
+    document.add_paragraph(e5.get('1.0', END))
+    document.add_heading('WORK EXPERIENCE')
+
+    if e6.get() != '':
+        document.add_paragraph().add_run(e7.get() + '-' + e8.get()).italic = True
+        document.add_paragraph().add_run(e6.get()).bold = True
+        document.add_paragraph(e9.get('1.0', END))
+    if e10.get() != '':
+        document.add_paragraph().add_run(e11.get() + '-' + e12.get()).italic = True
+        document.add_paragraph().add_run(e10.get()).bold = True
+        document.add_paragraph(e13.get('1.0', END))
+    if e14.get() != '':
+        document.add_paragraph().add_run(e15.get() + '-' + e16.get()).italic = True
+        document.add_paragraph().add_run(e14.get()).bold = True
+        document.add_paragraph(e17.get('1.0', END))
+    document.save('CV-app.docx')
+
+
+root = Tk()
+root.title('CV maker')
+root.geometry('650x700')
+photo = Label(root, text='Upload photo \n (only the path):').grid(row=0)
+e1 = Entry(root, width=60)
+e1.grid(row=0, column=1)
+name = Label(root, text='Full name:').grid(row=1)
+e2 = Entry(root, width=40)
+e2.grid(row=1, column=1)
+tel = Label(root, text='Mobile:').grid(row=2)
+e3 = Entry(root, width=40)
+e3.grid(row=2, column=1)
+email = Label(root, text='Email:').grid(row=3)
+e4 = Entry(root, width=40)
+e4.grid(row=3, column=1)
+about_me = Label(root, text='Describe yourself:').grid(row=4)
+e5 = Text(root, width=40, height=5)
+e5.grid(row=4, column=1)
+
+work_experience = Label(root, text='At which company?').grid(row=5)
+e6 = Entry(root, width=40)
+e6.grid(row=5, column=1)
+from_date = Label(root, text='From:   ').grid(row=6)
+e7 = Spinbox(root, from_=1980, to=2020, width=8)
+e7.grid(row=6, column=1)
+to_date = Label(root, text='To:   ').grid(row=7)
+e8 = Spinbox(root, from_=1980, to=2020, width=8)
+e8.grid(row=7, column=1)
+tasks = Label(root, text='Main tasks and \n responsibilities?   ').grid(row=8)
+e9 = Text(root, width=40, height=5)
+e9.grid(row=8, column=1)
+
+work_experience2 = Label(root, text='At which company?').grid(row=9)
+e10 = Entry(root, width=40)
+e10.grid(row=9, column=1)
+from_date = Label(root, text='From:   ').grid(row=10)
+e11 = Spinbox(root, from_=1980, to=2020, width=8)
+e11.grid(row=10, column=1)
+to_date = Label(root, text='To:   ').grid(row=11)
+e12 = Spinbox(root, from_=1980, to=2020, width=8)
+e12.grid(row=11, column=1)
+tasks = Label(root, text='Main tasks and \n responsibilities?   ').grid(row=12)
+e13 = Text(root, width=40, height=5)
+e13.grid(row=12, column=1)
+
+work_experience3 = Label(root, text='At which company?').grid(row=13)
+e14 = Entry(root, width=40)
+e14.grid(row=13, column=1)
+from_date = Label(root, text='From:   ').grid(row=14)
+e15 = Spinbox(root, from_=1980, to=2020, width=8)
+e15.grid(row=14, column=1)
+to_date = Label(root, text='To:   ').grid(row=15)
+e16 = Spinbox(root, from_=1980, to=2020, width=8)
+e16.grid(row=15, column=1)
+tasks = Label(root, text='Main tasks and \n responsibilities?   ').grid(row=16)
+e17 = Text(root, width=40, height=5)
+e17.grid(row=16, column=1)
+
+b = Button(root, text='commit', command=command)
+b.grid(row=17, column=1)
+
+root.mainloop()
+
+
+'''document = Document()
+print('Your cv-generator welcomes you, please answer all the questions below in order'
+      ' to get as your formatted cv document. :-)')
+
+try:
+    # we start with inserting a required size picture
+    document.add_picture(input('Type the full path of your passport-size picture: '),
+                         width=Inches(1.5))
+
+    name = input('what is your name: ')
+    phone = input('What is your phone number: ')
+    email = input('...and your email address is: ')
+
+    document.add_paragraph(name + '\n' + 'Mobile: ' + phone +
+                           '\n' + 'Email: ' + email)
+
+    # ABOUT ME
+    document.add_heading('ABOUT ME')
+
+    me = input('Tell me a little bit about yourself...')
+
+    document.add_paragraph(me)
+
+    # From this section there are a few differences between this version and the previous one,
+    # that is why I decided to create a modified version.
+
+    # WORK EXPERIENCE
+    document.add_heading('WORK EXPERIENCE')
+    p = document.add_paragraph()
+    company = ''
+    from_date = ''
+    to_date = ''
+    tasks = ''
+
+    anything_more = ''
+
+    while anything_more.lower() != 'n':
+        company = input('Type your previous company(ies) you work with: ')
+        from_date = input('From what year you started to work there? ')
+        to_date = input('When did you leave that company? ')
+        tasks = input('What were your main tasks and responsibilities? ')
+        p.add_run(from_date + '-' + to_date + '\n').italic = True
+        p.add_run(company + '\n').bold = True
+        p.add_run(tasks + '\n' + '\n')
+        anything_more = input('Is there anything more experience you\'d like '
+                              'to share? (y)es or (n)o? ')
+
+    document.save('emoke-CV.docx')
+    print('Your nice and simple CV has just been created. It\'s time to apply for a job.'
+          ' Good luck with that. :)')
+except BaseException:
+    print('Something wrong, please try again.')
+'''
